0608deb9d0df160c3ac6e761d5070fc856985d38
Alistair Reilly <allyreilly@icloud.com>
2021-12-11 11:48:32 -0800

    assigment submission

---
+++ assignments/working with basic data types/story_time.js
@@ -0,0 +1,32 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let title = "Cyber Harry Potter";
+let author = "Alistair Reilly";
+let myFirstName = "Harry";
+let mySecondName = "Potter";
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let year = "3800";         
+let age = "1870";
+let country = "New Russia";
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let weapons = ["cyber wand", "cyber staff"];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let didharrywin = true;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log(title);
+console.log("by " + author);
+console.log("The hero of our story is " + myFirstName + " " + mySecondName);
+console.log("This story begins in the year " + year + " the machines have taken over the rest of the world except for " + country); 
+console.log("The New Russians are creating super soldiers to win the war Harry Potter is the most advanced one yet it is up to him to end the machines hive mind and build humanity back up");
+ 
+console.log(myFirstName + " " + mySecondName + " must use his " + weapons[0] + " to hack into the machines and turn them to his side and destroy the hivemind");
+console.log("Harry Potter will also use his " + weapons[1] + " to deactivate the machines connections to the hive mind");
+console.log("Did Harry win? " + didharrywin);
